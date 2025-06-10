3fcae5c7f430c0d09afea2913c8ef24d0efbaf80
GLOGLOGLO3355 <rodrigomurga@gmail.com>
2025-06-10 19:39:19 -0400

    Fixed some display issues on sunburst chart

--- PreRelease/src/sunburst.py
+++ PreRelease/src/sunburst.py
@@ -4,15 +4,17 @@ import preprocess
 def get_sunburst():
     df = preprocess.load_data()
 
-    df = df.dropna(subset=["Parental_Education_Level", "Parental_Involvement"])
+    df["Parental_Education_Level"] = df["Parental_Education_Level"].fillna("Other")
+    df["Parental_Involvement"] = df["Parental_Involvement"].fillna("Other")
 
     fig = px.sunburst(
         df,
         path=["Parental_Education_Level", "Parental_Involvement"],
-        title="Parental Involvement by Education Level",
-        color="Parental_Involvement"
+        title="Parental Involvement by Education Level, click on a category to select it",
+        color="Parental_Involvement",
+        hover_data={"Parental_Involvement": False, "Parental_Education_Level": False}
     )
-
+    fig.update_traces(hovertemplate='%{label}<br>Count=%{value}<extra></extra>')
     fig.update_layout(margin=dict(t=40, l=0, r=0, b=0))
     return fig
 
