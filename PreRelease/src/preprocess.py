463611f702b0ac74786f215ae9e116393b1c1ca2
GLOGLOGLO3355 <rodrigomurga@gmail.com>
2025-06-14 13:32:59 -0400

    Came back to last function

--- PreRelease/src/preprocess.py
+++ PreRelease/src/preprocess.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-data_path = os.path.join(BASE_DIR, "assets", "data", "StudentPerformanceFactors.csv")
-df = pd.read_csv(data_path, sep=";")
+def load_data():
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    data_path = os.path.join(BASE_DIR, "assets", "data", "StudentPerformanceFactors.csv")
+    return pd.read_csv(data_path, sep=";")
